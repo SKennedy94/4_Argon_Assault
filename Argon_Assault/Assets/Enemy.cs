8f1f73f893b7e7402b5e1dc9c6bb5c6b43846028
Seamus Kennedy <31747877+SKennedy94@users.noreply.github.com>
2018-07-05 22:43:49 -0700

    particle Collisions

---
+++ Argon_Assault/Assets/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnParticleCollision(GameObject other)
+    {
+        StartDeathSequence();
+        print("particles collided with enemy: " + gameObject.name);
+    }
+
+    private void StartDeathSequence()
+    {
+        Destroy(gameObject);
+    }
+}
